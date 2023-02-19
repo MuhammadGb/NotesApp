c8e8de620e3391f408c4b8f60cca065a4da1329a
Muhammad <muhammad17gb@gmail.com>
2023-02-19 19:57:04 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 require("dotenv").config(); // loads the environment variables into process.env
-const path = require('path');
+const path = require("path");
 const cors = require("cors");
 
-
-const notesRouter = require('./routers/notes');
-const usersRouter = require('./routers/users');
-const { handleError } = require('./utils/error');
-const auth = require('./middleware/auth.js');
+const notesRouter = require("./routers/notes");
+const usersRouter = require("./routers/users");
+const { handleError } = require("./utils/error");
+const auth = require("./middleware/auth.js");
 
 // Configuring the database
-const mongoose = require('mongoose');
-const localUrl =  'mongodb://localhost:27017/simple-notes'
+const mongoose = require("mongoose");
+const localUrl = "mongodb://localhost:27017/simple-notes";
 
 const app = express();
 
@@ -21,11 +20,10 @@ app.use(cors());
 app.use((req, res, next) => {
   const { method, path } = req;
   console.log(
-  `New request to: ${method} ${path} at ${new Date().toISOString()}`
+    `New request to: ${method} ${path} at ${new Date().toISOString()}`,
   );
   next();
 });
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -34,26 +32,29 @@ app.use(auth.initialize());
 
 //Redirect the base URL / to /api/v1/recipes#
 app.get("/", (req, res) => {
-  res.redirect("/api/v1/recipes");
+  res.redirect("/api/v1/notes");
 });
 
 // Mongoose Connection
 mongoose.Promise = global.Promise;
 const { MONGO_URI } = process.env;
-const URL = MONGO_URI||localUrl
+const URL = MONGO_URI || localUrl;
 
 // Connecting to the database
-mongoose.connect(URL, {
+mongoose
+  .connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+  })
+  .then(() => {
     console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
     process.exit();
-});
+  });
 
-app.use('/api/v1/notes', notesRouter);
+app.use("/api/v1/notes", notesRouter);
 app.use("/api/v1/users", usersRouter);
 
 app.use(handleError);
